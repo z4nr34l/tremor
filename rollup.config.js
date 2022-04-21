cc95867fbb81d8c7bd716b6920f324eaaa052aec
mitrotasios <a.mitrotasios13@gmail.com>
2022-04-21 15:53:22 +0100

    exclude tests and stories from dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,28 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
-import dts from "rollup-plugin-dts";
+import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
     {
-        input: "src/index.ts",
+        input: 'src/index.ts',
         output: [
             {
                 file: packageJson.main,
-                format: "cjs",
+                format: 'cjs',
                 sourcemap: false,
             },
             {
                 file: packageJson.module,
-                format: "esm",
+                format: 'esm',
                 sourcemap: false,
             },
         ],
@@ -30,16 +30,19 @@ export default [
             peerDepsExternal(),
             resolve(),
             commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
+            typescript({ 
+                tsconfig: './tsconfig.json',
+                exclude: ['**/stories/**', '**/tests/**']
+            }),
             postcss(),
             terser(),
             typescriptPaths()
         ],
-  },
-  {
-      input: "dist/esm/types/index.d.ts",
-      output: [{ file: "dist/index.d.ts", format: "esm" }],
-      plugins: [dts()],
-      external: [/\.css$/],
-  },
+    },
+    {
+        input: 'dist/esm/types/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+        plugins: [dts()],
+        external: [/\.css$/],
+    },
 ];
