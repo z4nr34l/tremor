925d5de6d6a55f34ae0d9c971348d1f4ed55ba4e
mitrotasios <a.mitrotasios13@gmail.com>
2022-04-24 12:47:55 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    testEnvironment: "jsdom",
+    testEnvironment: 'jsdom',
     moduleNameMapper: {
-        ".(css|less|scss)$": "identity-obj-proxy",
-        "components/(.*)": "<rootDir>/src/components/$1",
-        "^@utils/(.*)": "<rootDir>/src/utils/$1",
-        "^@common/(.*)": "<rootDir>/src/components/common/$1"
+        '.(css|less|scss)$': 'identity-obj-proxy',
+        'components/(.*)': '<rootDir>/src/components/$1',
+        '^@utils/(.*)': '<rootDir>/src/utils/$1',
+        '^@common/(.*)': '<rootDir>/src/components/common/$1'
     },
 };
