8f81691a087f017b3cd4cfa5d939c75f5285a2a2
mitrotasios <a.mitrotasios13@gmail.com>
2022-04-30 12:38:58 +0300

    fix template literals for grid

--- src/components/layout-elements/ColGrid/ColGrid.tsx
+++ src/components/layout-elements/ColGrid/ColGrid.tsx
@@ -9,6 +9,21 @@ export interface ColGridProps {
     children: React.ReactNode
 }
 
+const GridCols: {[key: number]: string} = {
+    1: 'grid-cols-1',
+    2: 'grid-cols-2',
+    3: 'grid-cols-3',
+    4: 'grid-cols-4',
+    5: 'grid-cols-5',
+    6: 'grid-cols-6',
+    7: 'grid-cols-7',
+    8: 'grid-cols-8',
+    9: 'grid-cols-9',
+    10: 'grid-cols-10',
+    11: 'grid-cols-11',
+    12: 'grid-cols-12',
+};
+
 const ColGrid = ({
     numColumns = 3,
     gapX = 'gap-x-1',
@@ -17,6 +32,7 @@ const ColGrid = ({
 }: ColGridProps) => {
     return(
         <div className={ classNames(
+            GridCols[numColumns] ? GridCols[numColumns] : 'grid-cols-none',
             parseGapXClassNames(gapX),
             parseGapYClassNames(gapY),
             `grid-cols-${String(numColumns)}`,
--- src/components/layout-elements/RowGrid/RowGrid.tsx
+++ src/components/layout-elements/RowGrid/RowGrid.tsx
@@ -9,6 +9,15 @@ export interface RowGridProps {
     children: React.ReactNode
 }
 
+const GridRows: {[key: number]: string} = {
+    1: 'grid-rows-1',
+    2: 'grid-rows-2',
+    3: 'grid-rows-3',
+    4: 'grid-rows-4',
+    5: 'grid-rows-5',
+    6: 'grid-rows-6',
+};
+
 const RowGrid = ({
     numRows = 3,
     gapX = 'gap-x-1',
@@ -17,6 +26,7 @@ const RowGrid = ({
 }: RowGridProps) => {
     return(
         <div className={ classNames(
+            GridRows[numRows] ? GridRows[numRows] : 'grid-rows-none',
             parseGapXClassNames(gapX),
             parseGapYClassNames(gapY),
             `grid-rows-${String(numRows)}`,
