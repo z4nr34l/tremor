ee77a7bcce20c2f152580cbcbca3b29c5644b3ba
mitrotasios <a.mitrotasios13@gmail.com>
2022-06-16 21:06:16 +0300

    add colors to badge

--- src/components/icon-elements/Badge/Badge.tsx
+++ src/components/icon-elements/Badge/Badge.tsx
@@ -3,29 +3,32 @@ import React from 'react';
 import { badgeProportionsTextOnly, badgeProportionsWithIcon, iconProportions } from './mappings';
 import BadgeWrapper from '@common/BadgeWrapper';
 import { classNames } from '@utils/classname-utils';
+import { colors } from './mappings';
 
 export interface BadgeIconTextProps {
     text: string,
+    color?: string,
     size?: string,
     Icon?: React.ElementType
 }
 
 const Badge = ({
     text,
+    color = 'blue',
     Icon,
     size = 'sm',
 }: BadgeIconTextProps) => {
     const badgeProportions = Icon ? badgeProportionsWithIcon : badgeProportionsTextOnly;
     return(
         <BadgeWrapper 
-            {...badgeProportions[size]}
-            bgColor = {'bg-gray-100'}
-            textColor = {'text-gray-800'}
+            { ...badgeProportions[size] }
+            bgColor = { colors[color].bgColor }
+            textColor = { colors[color].textColor }
         >
             { Icon ? (
                 <Icon className={ classNames(
-                    iconProportions[size].margin || '',
-                    iconProportions[size].iconSize || '',
+                    iconProportions[size] ? iconProportions[size].margin! : iconProportions['sm'].margin!,
+                    iconProportions[size] ? iconProportions[size].iconSize! : iconProportions['sm'].iconSize!,
                 ) }
                 />
             ) : null }
---
+++ src/stories/icon-elements/Badge.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { BaseColors } from '@utils/objects';
+import { Sizes } from '@utils/objects';
+
+import Badge from 'components/icon-elements/Badge/Badge';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Tremor/IconElements/Badge',
+    component: Badge,
+} as ComponentMeta<typeof Badge>;
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+const Template: ComponentStory<typeof Badge> = (args) => (
+    <>
+        { Object.values(Sizes).map(size => (
+            <>
+                <Badge size={ size } text={ 'Live' } />
+            </>
+        )) }
+        { Object.values(BaseColors).map(color => (
+            <div>
+                <Badge color={ color } text={ 'Live' } />
+            </div>
+        )) }
+    </>
+);
+
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+};
