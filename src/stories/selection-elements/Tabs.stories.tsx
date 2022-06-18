12531a85ed353569900d84bcf676d965cee8cb1a
mitrotasios <a.mitrotasios13@gmail.com>
2022-06-18 17:57:11 +0300

    minor fixes

--- src/stories/selection-elements/Tabs.stories.tsx
+++ src/stories/selection-elements/Tabs.stories.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Tab from 'components/selection-elements/Tab/Tab';
 import TabList from 'components/selection-elements/TabList/TabList';
+
 import { BaseColors } from '@utils/objects';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
