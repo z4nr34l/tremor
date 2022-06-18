96778d8172c14a7ff203718efa332f7564c8a148
mitrotasios <a.mitrotasios13@gmail.com>
2022-06-18 19:26:21 +0300

    remove MetricReversed

--- src/components/text-elements/index.ts
+++ src/components/text-elements/index.ts
@@ -5,7 +5,6 @@ export { default as CallOut } from './CallOut';
 export { default as Caption } from './Caption';
 export { default as CategorySplit } from './CategorySplit';
 export { default as Metric } from './Metric';
-export { default as MetricReversed } from './MetricReversed';
 export { default as Subtitle } from './Subtitle';
 export { default as Text } from './Text';
 export { default as Title } from './Title';
