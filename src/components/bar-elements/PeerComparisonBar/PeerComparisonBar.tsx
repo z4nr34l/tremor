d905b719838bd5b2efa9f8c8c9aa9f0d9d8354da
mitrotasios <a.mitrotasios13@gmail.com>
2022-06-18 17:21:49 +0300

    peerComparison Bar

--- src/components/bar-elements/PeerComparisonBar/PeerComparisonBar.tsx
+++ src/components/bar-elements/PeerComparisonBar/PeerComparisonBar.tsx
@@ -4,34 +4,32 @@ import BaseComponentProps from '@common/BaseComponentInterface';
 
 import { classNames, parseBgClassNames, parseMarginTopClassNames } from '@utils/classname-utils';
 import BarWrapper from '@common/BarWrapper';
+import { BaseColors } from '@utils/objects';
+import { colors } from './mappings';
 
 export interface PeerComparisonBarProps extends BaseComponentProps {
     markerPercentage: number,
     peerGroupMinPercentage: number,
-    peerGroupMaxPercentage: number, 
-    markerBgColor?: string,
-    peerGroupBgColor?: string,
-    barBgColor?: string,
+    peerGroupMaxPercentage: number,
+    color?: string,
 }
 
 const PeerComparisonBar = ({
     markerPercentage,
     peerGroupMinPercentage,
     peerGroupMaxPercentage,
-    markerBgColor = 'bg-blue-500',
-    peerGroupBgColor = 'bg-gray-400',
-    barBgColor = 'bg-gray-200',
+    color = BaseColors.Blue,
     marginTop,
 }: PeerComparisonBarProps) => {
     return(
         <BarWrapper
-            bgColor={ classNames(parseBgClassNames(barBgColor)) }
+            bgColor={ colors[color].secondaryBgColor }
             marginTop={ parseMarginTopClassNames(marginTop) } 
         >
             <div className="h-full bg-transparent" style={ {'width': `${peerGroupMinPercentage}%`} } />
             <div
                 className={ classNames(
-                    parseBgClassNames(peerGroupBgColor),
+                    colors[color].primaryBgColor,
                     'h-full rounded-full'
                 ) }
                 style={ {'width': `${peerGroupMaxPercentage - peerGroupMinPercentage}%`} } 
@@ -41,7 +39,7 @@ const PeerComparisonBar = ({
                 style={ {'width': `${markerPercentage}%`} }
             >
                 <div className={ classNames(
-                    parseBgClassNames(markerBgColor),
+                    colors[color].markerBgColor,
                     'h-4 w-1 rounded-full ring-2 ring-white'
                 ) }
                 />
