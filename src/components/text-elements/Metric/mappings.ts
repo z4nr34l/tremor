fa06f3b3f867ef9d929a2e47c704071ab3da000a
mitrotasios <a.mitrotasios13@gmail.com>
2022-06-29 18:49:20 +0300

    darkText Metric

--- src/components/text-elements/Metric/mappings.ts
+++ src/components/text-elements/Metric/mappings.ts
@@ -8,69 +8,69 @@ export type MetricColorTypes = {
 
 export const colors: {[baseColor: string]: MetricColorTypes} = {
     'slate': {
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Slate].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Slate].darkText).textColor,
     },
     'gray': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Gray].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Gray].darkText).textColor,
     },
     'zinc': {
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Zinc].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Zinc].darkText).textColor,
     },
     'neutral': {
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Neutral].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Neutral].darkText).textColor,
     },
     'stone': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Stone].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Stone].darkText).textColor,
     },
     'red': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Red].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Red].darkText).textColor,
     },
     'orange': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Orange].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Orange].darkText).textColor,
     },
     'amber': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Amber].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Amber].darkText).textColor,
     },
     'yellow': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Yellow].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Yellow].darkText).textColor,
     },
     'lime': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Lime].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Lime].darkText).textColor,
     },
     'green': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Green].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Green].darkText).textColor,
     },
     'emerald': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Emerald].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Emerald].darkText).textColor,
     },
     'teal': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Teal].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Teal].darkText).textColor,
     },
     'cyan': {
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Cyan].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Cyan].darkText).textColor,
     },
     'sky': {
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Sky].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Sky].darkText).textColor,
     },
     'blue': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Blue].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Blue].darkText).textColor,
     },
     'indigo': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Indigo].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Indigo].darkText).textColor,
     },
     'violet': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Violet].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Violet].darkText).textColor,
     },
     'purple': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Purple].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Purple].darkText).textColor,
     },
     'fuchsia': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Fuchsia].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Fuchsia].darkText).textColor,
     },
     'pink': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Pink].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Pink].darkText).textColor,
     },
     'rose': { 
-        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Rose].text).textColor,
+        textColor: getColorVariantsFromColorThemeValue(colorTheme[BaseColors.Rose].darkText).textColor,
     }
 };
