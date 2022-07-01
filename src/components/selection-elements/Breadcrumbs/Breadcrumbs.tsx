e61cb5e4d38b18064cc773598df834938943a789
mitrotasios <a.mitrotasios13@gmail.com>
2022-07-01 22:53:13 +0300

    minor fix

--- src/components/selection-elements/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/selection-elements/Breadcrumbs/Breadcrumbs.tsx
@@ -83,7 +83,7 @@ const Breadcrumbs = ({
                                     className={ classNames(
                                         'text-gray-700',
                                         `group flex items-center justify-between px-4 py-2.5 space-x-10 w-full
-                                        sm:text-sm group-hover:text-gray-500 hover:bg-gray-100 text-left`
+                                        text-sm group-hover:text-gray-500 hover:bg-gray-100 text-left`
                                     ) }
                                 >
                                     { child }
