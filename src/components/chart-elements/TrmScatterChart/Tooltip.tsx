3f96f0263e585cc6c4490cf5af72275342e257a3
mitrotasios <a.mitrotasios13@gmail.com>
2022-07-04 22:34:27 +0300

    fix

--- src/components/chart-elements/TrmScatterChart/Tooltip.tsx
+++ src/components/chart-elements/TrmScatterChart/Tooltip.tsx
@@ -7,7 +7,6 @@ export interface ChartTooltipRowProps {
 }
 
 const ChartTooltipRow = ({ value, name }: ChartTooltipRowProps) => (
-    // @Achi: add suffix, e.g. km, cm, etc
     <div className="flex items-center justify-between space-x-8">
         <p className="text-gray-700 font-medium">{ value }</p>
         <p className="text-gray-500 font-normal">{ name }</p>
@@ -28,7 +27,7 @@ const ScatterChartTooltip = ({ active, payload, valueFormater }: ChartTooltipPro
                 <div className="py-2.5 px-4 space-y-1">
                     {
                         payload.map(({value, name}: { value: number, name: string }, idx: number) => (
-                            <ChartTooltipRow value={ valueFormater(value) } name={ name } key={ `id-${idx}` } />
+                            <ChartTooltipRow value={ String(value) } name={ name } key={ `id-${idx}` } />
                         ))
                     }
                 </div>
