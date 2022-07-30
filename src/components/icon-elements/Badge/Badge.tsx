2f42de6f401ea2c0dce3369748e0c795932e386a
mitrotasios <a.mitrotasios13@gmail.com>
2022-07-30 17:49:38 +0300

    icon-elements

--- src/components/icon-elements/Badge/Badge.tsx
+++ src/components/icon-elements/Badge/Badge.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { badgeProportionsTextOnly, badgeProportionsWithIcon, iconProportions } from './mappings';
+import { classNames, parseMarginTopClassNames } from '@utils/classname-utils';
 import BadgeWrapper from '@common/BadgeWrapper';
-import { classNames } from '@utils/classname-utils';
+import BaseComponentProps from '@common/BaseComponentInterface';
+import { Sizes } from '@utils/objects';
 import { colors } from './mappings';
 
-export interface BadgeIconTextProps {
+export interface BadgeIconTextProps extends BaseComponentProps {
     text: string,
     color?: string,
     size?: string,
@@ -16,24 +18,27 @@ const Badge = ({
     text,
     color = 'blue',
     Icon,
-    size = 'sm',
+    size = Sizes.SM,
+    marginTop,
 }: BadgeIconTextProps) => {
     const badgeProportions = Icon ? badgeProportionsWithIcon : badgeProportionsTextOnly;
     return(
-        <BadgeWrapper 
-            { ...badgeProportions[size] }
-            bgColor = { colors[color].bgColor }
-            textColor = { colors[color].textColor }
-        >
-            { Icon ? (
-                <Icon className={ classNames(
-                    iconProportions[size] ? iconProportions[size].margin! : iconProportions['sm'].margin!,
-                    iconProportions[size] ? iconProportions[size].iconSize! : iconProportions['sm'].iconSize!,
-                ) }
-                />
-            ) : null }
-            { text }
-        </BadgeWrapper>
+        <div className={ classNames(parseMarginTopClassNames(marginTop)) }>
+            <BadgeWrapper
+                { ...badgeProportions[size] }
+                bgColor = { colors[color].bgColor }
+                textColor = { colors[color].textColor }
+            >
+                { Icon ? (
+                    <Icon className={ classNames(
+                        iconProportions[size] ? iconProportions[size].margin! : iconProportions['sm'].margin!,
+                        iconProportions[size] ? iconProportions[size].iconSize! : iconProportions['sm'].iconSize!,
+                    ) }
+                    />
+                ) : null }
+                { text }
+            </BadgeWrapper>
+        </div>
     );
 };
 
--- src/components/icon-elements/BadgeDelta/BadgeDelta.tsx
+++ src/components/icon-elements/BadgeDelta/BadgeDelta.tsx
@@ -9,11 +9,12 @@ import {
     iconProportionsIconOnly,
     iconProportionsWithText,
 } from './mappings';
+import { classNames, parseMarginTopClassNames } from '@utils/classname-utils';
 import BadgeWrapper from '@common/BadgeWrapper';
-import { classNames } from '@utils/classname-utils';
+import BaseComponentProps from '@common/BaseComponentInterface';
 import { mapInputsToDeltaType } from '@utils/utils';
 
-export interface BadgeDeltaProps {
+export interface BadgeDeltaProps extends BaseComponentProps {
     delta?: string,
     deltaType: string,
     isIncreasePositive?: boolean,
@@ -24,24 +25,31 @@ const BadgeDelta = ({
     delta,
     deltaType,
     isIncreasePositive = true,
-    size = 'sm'
+    size = 'sm',
+    marginTop,
 }: BadgeDeltaProps) => {
     const Icon = deltaIcons[deltaType];
     const mappedDeltaType = mapInputsToDeltaType(deltaType, isIncreasePositive);
     const badgeProportions = delta ? badgeProportionsWithText : badgeProportionsIconOnly;
     return(
-        <BadgeWrapper
-            { ...badgeProportions[size] }
-            bgColor={ deltaBgColors[mappedDeltaType] }
-            textColor={ deltaTextColors[mappedDeltaType] }
-        >
-            <Icon className={ classNames(
-                delta ? (iconProportionsWithText[size].margin || '') : (iconProportionsIconOnly[size].margin || '') ,
-                delta ? (iconProportionsWithText[size].iconSize || '') : (iconProportionsIconOnly[size].iconSize || '')
-            ) }
-            />
-            { delta ? delta : null}
-        </BadgeWrapper>
+        <div className={ classNames(parseMarginTopClassNames(marginTop)) }>
+            <BadgeWrapper
+                { ...badgeProportions[size] }
+                bgColor={ deltaBgColors[mappedDeltaType] }
+                textColor={ deltaTextColors[mappedDeltaType] }
+            >
+                <Icon className={ classNames(
+                    delta
+                        ? (iconProportionsWithText[size].margin || '')
+                        : (iconProportionsIconOnly[size].margin || '') ,
+                    delta
+                        ? (iconProportionsWithText[size].iconSize || '')
+                        : (iconProportionsIconOnly[size].iconSize || '')
+                ) }
+                />
+                { delta ? delta : null}
+            </BadgeWrapper>
+        </div>
     );
 };
 
