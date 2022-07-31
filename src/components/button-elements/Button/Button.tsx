61c47b962c0aa9a34ed044aaf2fbb30f8a5968c4
mitrotasios <a.mitrotasios13@gmail.com>
2022-07-31 16:27:06 +0300

    add marginTop

--- src/components/button-elements/Button/Button.tsx
+++ src/components/button-elements/Button/Button.tsx
@@ -8,10 +8,11 @@ import {
     iconLeftProportions,
     iconRightProportions
 } from 'components/button-elements/Button/mappings';
+import { classNames, parseMarginTopClassNames } from '@utils/classname-utils';
+import BaseComponentProps from '@common/BaseComponentInterface';
 import ButtonWrapper from '@common/ButtonWrapper';
-import { classNames } from '@utils/classname-utils';
 
-export interface ButtonProps {
+export interface ButtonProps extends BaseComponentProps {
     text: string,
     Icon?: React.ElementType,
     iconPosition?: string,
@@ -31,45 +32,48 @@ const Button = ({
     size = Sizes.SM,
     color = BaseColors.Blue,
     importance = 'primary',
+    marginTop,
 }: ButtonProps) => {
     return(
-        <ButtonWrapper
-            onClick={ handleClick }
-            info={ tooltip }
-            { ...buttonProportions[size] }
-            { ...buttonShape }
-            { ...buttonColors[color][importance] }
-        >
-            { Icon && (iconPosition !== 'right') ? ( // ensures that icon is rendered if iconPosition is misspelled
-                <Icon
-                    className={classNames(
-                        iconLeftProportions[size]
-                            ? iconLeftProportions[size].margin!
-                            : iconLeftProportions['md'].margin!,
-                        iconLeftProportions[size]
-                            ? iconLeftProportions[size].iconSize!
-                            : iconLeftProportions['md'].iconSize!,
-                    )}
-                    aria-hidden="true"
-                />
-            ) : null }
-            <span className="whitespace-nowrap">
-                { text }
-            </span>
-            { Icon && (iconPosition === 'right') ? (
-                <Icon
-                    className={classNames(
-                        iconRightProportions[size]
-                            ? iconRightProportions[size].margin!
-                            : iconRightProportions['md'].margin!,
-                        iconRightProportions[size]
-                            ? iconRightProportions[size].iconSize!
-                            : iconRightProportions['md'].iconSize!,
-                    )}
-                    aria-hidden="true"
-                />
-            ) : null }
-        </ButtonWrapper>
+        <div className={ classNames(parseMarginTopClassNames(marginTop)) }>
+            <ButtonWrapper
+                onClick={ handleClick }
+                info={ tooltip }
+                { ...buttonProportions[size] }
+                { ...buttonShape }
+                { ...buttonColors[color][importance] }
+            >
+                { Icon && (iconPosition !== 'right') ? ( // ensures that icon is rendered if iconPosition is misspelled
+                    <Icon
+                        className={classNames(
+                            iconLeftProportions[size]
+                                ? iconLeftProportions[size].margin!
+                                : iconLeftProportions['md'].margin!,
+                            iconLeftProportions[size]
+                                ? iconLeftProportions[size].iconSize!
+                                : iconLeftProportions['md'].iconSize!,
+                        )}
+                        aria-hidden="true"
+                    />
+                ) : null }
+                <span className="whitespace-nowrap">
+                    { text }
+                </span>
+                { Icon && (iconPosition === 'right') ? (
+                    <Icon
+                        className={classNames(
+                            iconRightProportions[size]
+                                ? iconRightProportions[size].margin!
+                                : iconRightProportions['md'].margin!,
+                            iconRightProportions[size]
+                                ? iconRightProportions[size].iconSize!
+                                : iconRightProportions['md'].iconSize!,
+                        )}
+                        aria-hidden="true"
+                    />
+                ) : null }
+            </ButtonWrapper>
+        </div>
     );
 };
 
--- src/components/button-elements/ButtonInline/ButtonInline.tsx
+++ src/components/button-elements/ButtonInline/ButtonInline.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import { BaseColors, Sizes } from '@utils/objects';
 import { buttonColors, buttonProportions, buttonShape, iconLeftProportions, iconRightProportions } from './mappings';
+import { classNames, parseMarginTopClassNames } from '@utils/classname-utils';
+import BaseComponentProps from '@common/BaseComponentInterface';
 import ButtonWrapper from '@common/ButtonWrapper';
-import { classNames } from '@utils/classname-utils';
 
-export interface ButtonInlineProps {
+export interface ButtonInlineProps extends BaseComponentProps {
     text: string,
     Icon?: React.ElementType,
     iconPosition?: string,
@@ -23,43 +24,46 @@ const ButtonInline = ({
     tooltip,
     size = Sizes.SM,
     color = BaseColors.Blue,
+    marginTop,
 }: ButtonInlineProps) => {
     return(
-        <ButtonWrapper
-            onClick={ handleClick }
-            info={ tooltip }
-            { ...buttonProportions[size] }
-            { ...buttonShape }
-            { ...buttonColors[color] }
-        >
-            { Icon && (iconPosition !== 'right') ? ( // ensures that icon is rendered if iconPosition is misspelled
-                <Icon
-                    className={ classNames(
-                        iconLeftProportions[size]
-                            ? iconLeftProportions[size].margin!
-                            : iconLeftProportions['md'].margin!,
-                        iconLeftProportions[size]
-                            ? iconLeftProportions[size].iconSize!
-                            : iconLeftProportions['md'].iconSize!,
-                    ) }
-                    aria-hidden="true"
-                />
-            ) : null }
-            { text }
-            { Icon && (iconPosition === 'right') ? (
-                <Icon
-                    className={ classNames(
-                        iconRightProportions[size]
-                            ? iconRightProportions[size].margin!
-                            : iconRightProportions['md'].margin!,
-                        iconRightProportions[size]
-                            ? iconRightProportions[size].iconSize!
-                            : iconRightProportions['md'].iconSize!,
-                    ) }
-                    aria-hidden="true"
-                />
-            ) : null }
-        </ButtonWrapper>
+        <div className={ classNames(parseMarginTopClassNames(marginTop)) }>
+            <ButtonWrapper
+                onClick={ handleClick }
+                info={ tooltip }
+                { ...buttonProportions[size] }
+                { ...buttonShape }
+                { ...buttonColors[color] }
+            >
+                { Icon && (iconPosition !== 'right') ? ( // ensures that icon is rendered if iconPosition is misspelled
+                    <Icon
+                        className={ classNames(
+                            iconLeftProportions[size]
+                                ? iconLeftProportions[size].margin!
+                                : iconLeftProportions['md'].margin!,
+                            iconLeftProportions[size]
+                                ? iconLeftProportions[size].iconSize!
+                                : iconLeftProportions['md'].iconSize!,
+                        ) }
+                        aria-hidden="true"
+                    />
+                ) : null }
+                { text }
+                { Icon && (iconPosition === 'right') ? (
+                    <Icon
+                        className={ classNames(
+                            iconRightProportions[size]
+                                ? iconRightProportions[size].margin!
+                                : iconRightProportions['md'].margin!,
+                            iconRightProportions[size]
+                                ? iconRightProportions[size].iconSize!
+                                : iconRightProportions['md'].iconSize!,
+                        ) }
+                        aria-hidden="true"
+                    />
+                ) : null }
+            </ButtonWrapper>
+        </div>
     );
 };
 
