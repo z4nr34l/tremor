c2c21875f6908e69180fbbc12143962c81adf8ea
mitrotasios <a.mitrotasios13@gmail.com>
2022-08-06 16:19:48 +0300

    Update index.ts

--- src/components/icon-elements/index.ts
+++ src/components/icon-elements/index.ts
@@ -1,5 +1,4 @@
 export { default as Badge } from './Badge';
 export { default as BadgeDelta } from './BadgeDelta';
-export { default as BadgeStatus } from './BadgeStatus';
 
 export { default as Icon } from './Icon';
