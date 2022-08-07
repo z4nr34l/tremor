0cdafc567f5048a29eaacd59ed81bae92a62b31f
mitrotasios <a.mitrotasios13@gmail.com>
2022-08-07 10:09:50 +0300

    Update TextBar.stories.tsx

--- src/stories/bar-elements/TextBar.stories.tsx
+++ src/stories/bar-elements/TextBar.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import TextBar from 'components/bar-elements/TextBar/TextBar';
+import { TextBar } from 'components';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
