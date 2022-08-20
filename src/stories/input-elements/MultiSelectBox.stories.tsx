5be087ef5ee90f96f1755a82ee00a5ea7db9835f
mitrotasios <a.mitrotasios13@gmail.com>
2022-08-20 23:53:03 +0300

    MultiSelectBox button

--- src/stories/input-elements/MultiSelectBox.stories.tsx
+++ src/stories/input-elements/MultiSelectBox.stories.tsx
@@ -4,6 +4,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import MultiSelectBox from 'components/input-elements/MultiSelectBox/MultiSelectBox';
 import MultiSelectBoxItem from 'components/input-elements/MultiSelectBox/MultiSelectBoxItem';
+import { SelectBox, SelectBoxItem } from 'components';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -13,11 +14,19 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 
 const Template: ComponentStory<typeof MultiSelectBox> = () => (
-    <MultiSelectBox>
-        <MultiSelectBoxItem name="Option 1 ABCSASDASDASDASASDASDASDSASDASABCSASDASDASDASASDASDASDSASDAS" value={ 1 } />
-        <MultiSelectBoxItem name="Option 2" value={ 2 } />
-        <MultiSelectBoxItem name="Option 3" value={ 3 } />
-    </MultiSelectBox>
+    <div>
+        <MultiSelectBox>
+            <MultiSelectBoxItem name="Option 1 ABCSASDASDASDASASDASDASDSASDASABCSASDASDASDASASDASDASDSASDAS" value={ 1 } />
+            <MultiSelectBoxItem name="Option 2" value={ 2 } />
+            <MultiSelectBoxItem name="Option 3" value={ 3 } />
+        </MultiSelectBox>
+        <SelectBox handleSelect={ (value) => console.log('the new value is', value) } marginTop="mt-1">
+            <SelectBoxItem  value={1} name="Option One ABCSASDASDASDASASDASDASDSASDASABCSASDASDASDASASDASDASDSASDAS" />
+            <SelectBoxItem  value={2} name="Option Two" />
+            <SelectBoxItem  value={3} name="Option Three" />
+        </SelectBox>
+    </div>
+
 );
   
 export const Default = Template.bind({});
