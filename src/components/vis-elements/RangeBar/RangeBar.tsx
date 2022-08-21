ce1e191cebd90546c2d411b857097f1bb8fcbe9c
mitrotasios <a.mitrotasios13@gmail.com>
2022-08-21 19:09:25 +0300

    Update RangeBar.tsx

--- src/components/vis-elements/RangeBar/RangeBar.tsx
+++ src/components/vis-elements/RangeBar/RangeBar.tsx
@@ -6,6 +6,7 @@ import Tooltip from '@tippyjs/react';
 import { classNames, getColorVariantsFromColorThemeValue, parseMarginTopClassNames } from 'lib/classnameUtils';
 import colorTheme, { defaultColors } from 'lib/colorTheme';
 import { BaseColors } from 'lib/primitives';
+import { sizing } from 'lib/sizing';
 
 export interface RangeBarProps {
     percentageValue: number,
@@ -29,14 +30,15 @@ const RangeBar = ({
     return(
         <div className={ classNames(parseMarginTopClassNames(marginTop)) }>
             <div className={ classNames(
-                'relative h-2 w-full flex rounded-lg items-center',
+                'relative flex items-center w-full rounded-lg',
                 getColorVariantsFromColorThemeValue(defaultColors.lightBackground).bgColor,
+                sizing.xs.height,
             ) }>
                 <Tooltip content={ rangeTooltip } className={ rangeTooltip ? '' : 'hidden' }>
                     <div
                         className={ classNames(
+                            'absolute h-full rounded-lg',
                             getColorVariantsFromColorThemeValue(defaultColors.darkBackground).bgColor,
-                            'absolute h-full rounded-full'
                         ) }
                         style={ {'left': `${minRangeValue}%`, 'width': `${maxRangeValue - minRangeValue}%`} } 
                     />
@@ -45,9 +47,11 @@ const RangeBar = ({
                     <Tooltip content={ markerTooltip } className={ markerTooltip ? '' : 'hidden' }>
                         <div
                             className={ classNames(
-                                'h-4 w-1 rounded-lg ring-2 ring-white',
-                                'right-1/2 -translate-x-1/2',
+                                'rounded-lg ring-2 right-1/2 -translate-x-1/2',
                                 getColorVariantsFromColorThemeValue(colorTheme[color].background).bgColor,
+                                getColorVariantsFromColorThemeValue(defaultColors.white).ringRolor,
+                                sizing.md.height,
+                                sizing.twoXs.width,
                             ) }
                         />
                     </Tooltip>
