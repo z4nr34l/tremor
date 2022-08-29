57884a6701411200a529c585744349e511fb7802
mitrotasios <a.mitrotasios13@gmail.com>
2022-08-29 18:32:07 +0300

    add Block component

---
+++ src/components/layout-elements/Block/Block.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { MarginTop, SpaceY, TextAlignment } from '../../../lib';
+import {  TextAlignments, classNames, parseTruncateOption } from 'lib';
+
+export interface BlockProps {
+    spaceY?: SpaceY,
+    textAlignment: TextAlignment,
+    truncate?: boolean,
+    marginTop?: MarginTop,
+    children: React.ReactNode,
+}
+
+const Block = ({
+    spaceY = 'space-y-0',
+    textAlignment = TextAlignments.Left,
+    truncate = false,
+    marginTop = 'mt-0',
+    children
+}: BlockProps) => {
+    return(
+        <div className={ classNames(
+            'w-full',
+            spaceY,
+            textAlignment,
+            parseTruncateOption(truncate),
+            truncate ? 'whitespace-nowrap' : '',
+            marginTop,
+        ) }
+        >
+            { children }
+        </div>
+    );
+};
+
+export default Block;
